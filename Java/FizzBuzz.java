cb9179eb6993661b0fa2bb8250946faff25e683e
Chris Mayol <drlilchris@gmail.com>
2019-09-19 23:23:19 -0400

    added FizzBuzz

---
+++ Java/FizzBuzz.java
@@ -0,0 +1,25 @@
+import java.util.*;
+/*
+Write a program that outputs the string representation of numbers from 1 to n.
+But for multiples of three it should output “Fizz” instead of the number 
+and for the multiples of five output “Buzz”. 
+For numbers which are multiples of both three and five output “FizzBuzz”.
+*/ 
+class FizzBuzz{
+
+    static void fizzbuzz(int n){
+        for(int i=1; i<=n; i++){
+            if(i%3==0 && i%5==0)
+                System.out.println("FizzBuzz");
+            else if(i%5==0)
+                System.out.println("Buzz");
+            else if(i%3==0)
+                System.out.println("Fizz");
+            else 
+                System.out.println(i);
+        }
+    }
+    public static void main(String[] args){
+        fizzbuzz(30);
+    }
+}
