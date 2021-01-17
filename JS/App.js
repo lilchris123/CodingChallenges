402d1fa75ae26ac9a55c71909032d654bb64c297
Chris Mayol <drlilchris@gmail.com>
2021-01-16 19:33:23 -0500

    app component styling, added count state comp

--- JS/App.js
+++ JS/App.js
@@ -8,20 +8,13 @@ import RandomUsers from './containers/RandomUsers';
 import InputExample from "./containers/InputExample";
 import InputRefExample from "./containers/InputRefExample";
 import FunctionalInputExample from "./containers/functionalInputExample";
-import BinarySearchTreeNode from './dataStructures/tree/BinarySearchTree/BinarySearchTreeNode';
-import {secLargestNode} from './challenges/daily/SecLargestBSTNode36';
-const App = () => {
-  let root= new BinarySearchTreeNode(10);
-  root.insert(5);
-  //root.insert(19);
-  root.insert(2);
-  root.insert(7);
+import "./App.css";
 
-  
+const App = () => {
   return (
     <div>
       <h2>React Container</h2>
-      <h3>{secLargestNode(root)}</h3>
+      <InputExample/>
     </div>
   );
 };
--- JS/containers/InputExample.js
+++ JS/containers/InputExample.js
@@ -4,12 +4,17 @@ import InputUser from '../components/InputUser';
 export default class InputExample extends Component{
     constructor(){
         super();
-        this.state={user: ''};
+        this.state={user: '',count: 0};
         this.handleChange=this.handleChange.bind(this);
     }
 
     handleChange(event){
         this.setState({user: event.target.value});
+        this.setState(prevState => {
+            return {
+                count: prevState.count +1
+            }
+        });
     }
 
     render(){
