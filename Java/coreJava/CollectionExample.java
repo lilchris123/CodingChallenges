247cfc716a45f18f227e0e84fb66b7f514c268a8
Chris Mayol <drlilchris@gmail.com>
2021-02-03 23:39:07 -0500

    added more collection examples

--- Java/coreJava/CollectionExample.java
+++ Java/coreJava/CollectionExample.java
@@ -4,7 +4,20 @@ import java.util.*;
 class CollectionExample{
 
     public static void collectionEx(){
+        Stack<Integer> stack= new Stack<>();
+        stack.add(5);
+        stack.add(4);
 
+        Queue<Integer> queue= new LinkedList<>();
+        queue.add(4);
+        queue.poll();
+
+        Deque<Integer> deque= new LinkedList<>();
+        deque.add(5);
+        deque.add(9);
+        deque.poll();
+        deque.pop();
+    
         Set<Integer> set= new TreeSet<Integer>();
         //set.add(null);
         set.add(4);
